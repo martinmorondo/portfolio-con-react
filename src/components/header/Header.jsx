942545b1401bfd75a996e9fbaa1be90c34a041e5
Martín Morondo <martinmorondo@gmail.com>
2022-12-16 23:39:34 -0300

    add navigation bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,8 +10,38 @@ const Header = () => {
         <div className='nav-menu'>
           <ul className='nav-list-grid'>
             <li className='nav-item'>
-              <a href = "" className='nav-link'>
-                <i className='uil uil- nav-icon'></i>
+              <a href = "#home" className='nav-link'>
+                <i className='uil uil-estate nav-icon'></i>Home
+              </a>
+            </li>
+
+            <li className='nav-item'>
+              <a href = "#home" className='nav-link'>
+                <i className='uil uil-user nav-icon'></i> About
+              </a>
+            </li>
+
+            <li className='nav-item'>
+              <a href = "#skills" className='nav-link'>
+                <i className='uil uil-file-alt nav-icon'></i> Skills
+              </a>
+            </li>
+
+            <li className='nav-item'>
+              <a href = "#services" className='nav-link'>
+                <i className='uil uil-briefcase-alt nav-icon'></i> Services
+              </a>
+            </li>
+
+            <li className='nav-item'>
+              <a href = "#portfolio" className='nav-link'>
+                <i className='uil uil-scenery nav-icon'></i> Portfolio
+              </a>
+            </li>
+
+            <li className='nav-item'>
+              <a href = "#contact" className='nav-link'>
+                <i className='uil uil-message nav-icon'></i> Contact
               </a>
             </li>
           </ul>
