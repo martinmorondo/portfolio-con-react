a9840b47e1bf1d34f32a561ef784d747620b8e1f
Martín Morondo <martinmorondo@gmail.com>
2022-12-23 02:53:59 -0300

    Create a new component called 'ScrollDown'

---
+++ src/components/home/ScrollDown.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ScrollDown = () => {
+  return (
+    <div className='home-scroll'>
+        <a href='#about' className='home-scroll button button-flex'></a>
+    </div>
+  )
+}
+
+export default ScrollDown
