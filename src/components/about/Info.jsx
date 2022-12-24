9d67e52a025c915b6d3e98e30092f5aa245bb3d2
Martín Morondo <martinmorondo@gmail.com>
2022-12-24 00:38:39 -0300

    I added styles to the About.jsx component

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Info = () => {
   return (
-    <div></div>
+    <div className='about-info grid'>
+        <div className='about-box'>
+        <i class='bx bx-award about-icon'></i>
+            <h3 className='about-title'>Experience</h3>
+            <span className='about-subtitle'>1 year working</span>
+        </div>
+
+        <div className='about-box'>
+        <i class='bx bx-briefcase about-icon'></i>
+            <h3 className='about-title'>Completed</h3>
+            <span className='about-subtitle'>+48 projects</span>
+        </div>
+
+        <div className='about-box'>
+        <i class='bx bx-support about-icon'></i>
+            <h3 className='about-title'>Support</h3>
+            <span className='about-subtitle'>Online 24/7</span>
+        </div>
+
+        
+    </div>
   )
 }
 
