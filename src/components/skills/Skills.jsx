ff79d14c5126a56b079c16024371581eaa812406
Martín Morondo <martinmorondo@gmail.com>
2022-12-26 02:23:47 -0300

    Create the Frontend.jsx and Backend.jsx component

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,6 +5,8 @@ const Skills = () => {
     <section className='skills section' id='skills'>
         <h2 className='section-title'>Skills</h2>
       <span className='section-subtitle'>My technical level</span>
+
+      <div className='skills-contaner container grid'></div>
     </section>
     )
 }
