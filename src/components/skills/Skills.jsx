faf4229d37a541a3d8da53ff6990a743646f0e1a
Martín Morondo <martinmorondo@gmail.com>
2022-12-26 02:21:59 -0300

    I painted the Skills.jsx component in App.jsx

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <section className='skills section' id='skills'>
+        <h2 className='section-title'>Skills</h2>
+      <span className='section-subtitle'>My technical level</span>
+    </section>
+    )
+}
+
+export default Skills
